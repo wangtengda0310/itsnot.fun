c43e2767db7702dbfed78835472c38317d60d370
王腾达 <wangtengda0310@126.com>
2025-06-20 16:50:08 +0800

    start.ipfs.docker

---
+++ start.ipfs.docker.sh
@@ -0,0 +1,3 @@
+export ipfs_staging=/home/ipfs
+export ipfs_data=/home/ipfs
+docker run -d --name ipfs -v $ipfs_staging:/export -v $ipfs_data:/data/ipfs -p 4001:4001 -p 4001:4001/udp -p 127.0.0.1:8080:8080 -p 127.0.0.1:5001:5001 ipfs/kubo
